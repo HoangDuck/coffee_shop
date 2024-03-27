6139fb98515e8c8b7db0a35e0568c85b64df8392
duchoang2206 <duc.hoang@executionlab.asia>
2024-03-27 11:57:03 +0700

    remove assign delegate object

--- coffee_shop/Scenes/Onboarding/OnboardingCoordinator.swift
+++ coffee_shop/Scenes/Onboarding/OnboardingCoordinator.swift
@@ -24,8 +24,7 @@ class OnboardingCoordinator: NSObject, OnboardViewControllerDelegate {
     
     func start(){
         let storyBoardOnboard = UIStoryboard(name: "OnboardViewController", bundle: nil)
-        let onboardViewController = storyBoardOnboard.instantiateViewController(identifier: "OnboardViewController") as OnboardViewController
-        onboardViewController.delegate = self
+        let onboardViewController = storyBoardOnboard.instantiateViewController(identifier: "OnboardViewController")
         self.navigationController?.pushViewController(onboardViewController, animated: true)
     }
 }
